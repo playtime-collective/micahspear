9fe420f9795a4be7d1aea583c3e91c1918a8a597
sethvincent <sethvincent@gmail.com>
2012-05-28 22:13:40 +0000

    cleanup

--- wp-content/themes/woop/header.php
+++ wp-content/themes/woop/header.php
@@ -33,6 +33,86 @@
 
 <style>
 
+p, div {
+	font-family: tahoma, arial, sans-serif;
+	font-weight: 100;
+	font-size: 13px;
+	letter-spacing: 0.1em;
+	color: #666666;
+	line-height: 1.5em;
+}
+
+.topbar {
+	display: none;
+}
+
+nav ul {
+	padding-top: 8px;
+}
+
+nav a:link {
+	font-size: 15px;
+	font-weight: 300;
+	color: #4a4949;
+	text-transform: uppercase;
+	padding: 7px 30px 7px 30px;
+	line-height: 18px;
+}
+
+.subheader {
+	text-align: center;
+	margin-top: 70px;
+	margin-bottom: 50px;
+}
+
+div.twitter_wrap {
+  clear: both;
+  border: 1px solid #f5f5f5;
+  padding: 15px 0;
+  background-color: white;
+  margin-top: 25px;
+  width: 268px;
+}
+
+div#slides a.prev {
+  float: left;
+  padding-left: 6px;
+  margin-left: 22px;
+}
+
+span.ss_text {
+	color: #808080;
+	font-size: 13px;
+}
+
+footer p {
+	margin-top: 20px;
+	font-size: 10px;
+	color: #454545;
+	text-transform: uppercase;
+	padding-top: 30px;
+	letter-spacing: .4em;
+}
+
+.alignleft, img.alignleft {
+	margin-right: 10px;
+	display: inline;
+	float: left;
+	margin-top: 0px;
+}
+
+.skills {
+	width:200px;
+	float:left;
+	text-align:center;
+	padding:0px 20px 0px 20px;
+}
+
+.skills p {
+	font-size: 12px;
+	color:#666666;
+}
+
 </style>
 
 
